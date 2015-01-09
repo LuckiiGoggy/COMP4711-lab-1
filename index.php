e1e5c6038d242c103a6866ac15f811ded35f8f2e
LuckiiGoggy <luck-13@live.ca>
2015-01-08 17:28:29 -0800

    added index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    
+    <body>
+        <?php
+        
+        /* 
+         * To change this license header, choose License Headers in Project Properties.
+         * To change this template file, choose Tools | Templates
+         * and open the template in the editor.
+         */
+        
+        echo 'La dee da';
+        
+        ?>
+    </body>
+</html>
