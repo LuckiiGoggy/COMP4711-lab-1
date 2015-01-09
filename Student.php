3229ac8a2316a191dabc6fae0091d60058cbdd62
LuckiiGoggy <luck-13@live.ca>
2015-01-08 22:45:20 -0800

    Commented code for Student.php

--- Student.php
+++ Student.php
@@ -13,6 +13,12 @@
  */
 class Student {
     
+    //Constructor for Student
+    // sets 4 variables
+    // surname as a string
+    // first_name as a string
+    // emails as an array
+    // grades as an array
     function _construct(){
         $this->surname = '';
         $this->first_name = '';
@@ -20,14 +26,17 @@ class Student {
         $this->grades = array();
     }
     
+    // Function to add an email to the emails array
     function add_email($which, $address){
         $this->emails[$which] = $address;
     }
     
+    // Function to add a grade to the grades array
     function add_grade($grade){
         $this->grades[] = $grade;
     }
     
+    // Function to get the average grade of the student
     function average(){
         $total = 0;
         foreach($this->grades as $value)
@@ -35,6 +44,8 @@ class Student {
         return $total / count($this->grades);
     }
     
+    // Function to get the student's information as a string fit
+    // formatted in html
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
